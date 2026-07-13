6b64c77495ef2b10c240a4942dddc26d9422dc7c
Jennifer Fuentes <jenniferfuent12@gmail.com>
2026-07-12 20:53:26 -0600

    Merge remote-tracking branch 'origin/jenni' into dev

--- PHPCore/index.php
+++ PHPCore/index.php
@@ -3,21 +3,21 @@
 declare(strict_types=1);
 
 use App\Controllers\HomeController;
+use App\Controllers\Oferentes_controller;
 use App\Core\Sesion;
 
 require __DIR__.'/app/bootstrap.php';
 
 Sesion::start();
 
-$controller = new HomeController();
-
-
 $action = filter_input(INPUT_GET, 'action') ?: 'index';
 
 try {
     match ($action) {
-        'index' => $controller->index(),
-        default => $controller->notFound(),
+        'index'              => (new HomeController())->index(),
+        'oferentesPorPuesto' => (new Oferentes_controller())->oferentesPorPuesto(),
+        'listadoOferentes'   => (new Oferentes_controller())->listadoOferentes(),
+        default              => (new HomeController())->notFound(),
     };
 } catch (Throwable $exception) {
     http_response_code(500);
