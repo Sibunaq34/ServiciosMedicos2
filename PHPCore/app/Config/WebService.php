f71f283e93f03d54f9273f7baab884fb1618a9e9
Antony Cervantes <antonny22c.c@gmail.com>
2026-07-20 12:17:03 -0600

    Limpieza de codigo

--- PHPCore/app/Config/WebService.php
+++ PHPCore/app/Config/WebService.php
@@ -6,12 +6,8 @@ namespace App\Config;
 
 final class WebService
 {
-    public const LOGIN = 'http://localhost:8080/ServicioLogin.svc?wsdl';
-    public const USUARIO = 'http://localhost:8080/ServicioUsuario.svc?wsdl';
-    public const PACIENTE = 'http://localhost:8080/ServicioPaciente.svc?wsdl';
+
     public const EMPLEADOS = 'http://localhost:54784/ServicioEmpleados.svc?wsdl';
-    // Persona C - Kenneth
-    // Centraliza el WSDL utilizado por CORE8.
     public const DETALLE_OFERENTE = 'http://localhost:54784/ServicioDetalleOferente.svc?wsdl';
     public const OFERENTES = 'http://localhost:54784/ServicioOferentesPuesto.svc?wsdl';
 }
